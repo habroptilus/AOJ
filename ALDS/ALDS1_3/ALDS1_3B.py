449100a787151807f10675312ca2a40a987c81c8
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-05-26 22:32:39 +0900

    rename

--- ALDS/ALDS1_3/ALDS1_3B.py
+++ ALDS/ALDS1_3/ALDS1_3B.py
@@ -1,19 +1,17 @@
-# TLEになる。O(n)になっていない？
-
+from collections import deque
 n, q = map(int, input().split())
-queue = []
+queue = deque()
 for i in range(n):
     name, time = input().split()
     queue.append({"name": name, "time": int(time)})
 t = 0
 while len(queue) != 0:
     #print([queue[i]["time"] for i in range(len(queue))])
-    if queue[0]["time"] <= q:
-        t += queue[0]["time"]
-        name = queue[0]["name"]
-        queue.remove(queue[0])
+    head = queue.popleft()
+    if head["time"] <= q:
+        t += head["time"]
+        name = head["name"]
         print("{} {}".format(name, t))
     else:
-        queue = queue[1:] + \
-            [{"name": queue[0]["name"], "time":queue[0]["time"] - q}]
+        queue.append({"name": head["name"], "time": head["time"] - q})
         t += q
